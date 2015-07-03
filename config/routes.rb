6e3a1cb3eda3aff8a283b9ffd5ceadf56d5b5688
Alex Kelley <adkelley@mac.com>
2015-07-03 12:03:40 -0700

    bootstrap, navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'store_its#index'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
+
   resources :receipts
   resources :store_its
-  root 'store_its#index'
 end
